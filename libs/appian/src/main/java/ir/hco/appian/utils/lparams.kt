50d88d84472c6d8294746ca920304518e7d5a785
Hossain Khademian <hco@post.com>
2019-07-06 16:36:04 +0430

    add default values

--- libs/appian/src/main/java/ir/hco/appian/utils/lparams.kt
+++ libs/appian/src/main/java/ir/hco/appian/utils/lparams.kt
@@ -9,7 +9,7 @@ import android.widget.LinearLayout
 inline fun <T : View> T.lparams(
 	width: Int = android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
 	height: Int = android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-	init: ViewGroup.LayoutParams.() -> Unit
+	init: ViewGroup.LayoutParams.() -> Unit = {}
 ): T {
 	val layoutParams = ViewGroup.LayoutParams(width, height)
 	layoutParams.init()
@@ -20,7 +20,7 @@ inline fun <T : View> T.lparams(
 inline fun <T : View> T.frameLParams(
 	width: Int = android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
 	height: Int = android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-	init: FrameLayout.LayoutParams.() -> Unit
+	init: FrameLayout.LayoutParams.() -> Unit = {}
 ): T {
 	val layoutParams = FrameLayout.LayoutParams(width, height)
 	layoutParams.init()
@@ -31,7 +31,7 @@ inline fun <T : View> T.frameLParams(
 inline fun <T : View> T.linearLParams(
 	width: Int = android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
 	height: Int = android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-	init: LinearLayout.LayoutParams.() -> Unit
+	init: LinearLayout.LayoutParams.() -> Unit = {}
 ): T {
 	val layoutParams = LinearLayout.LayoutParams(width, height)
 	layoutParams.init()

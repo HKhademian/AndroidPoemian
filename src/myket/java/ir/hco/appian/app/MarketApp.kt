4df71c96f488678dafde6db2fb000a0913229735
Hossain Khademian <hco@post.com>
2019-08-01 19:19:24 +0430

    update ads

--- src/myket/java/ir/hco/appian/app/MarketApp.kt
+++ src/myket/java/ir/hco/appian/app/MarketApp.kt
@@ -1,9 +1,24 @@
 package ir.hco.appian.app
 
+import ir.hco.magnet.MagnetAdvertiser
+import ir.hco.util.Advertiser
 import ir.hco.util.MyketPublisher
 
 class MarketApp : MainApp() {
 	override val publisher = MyketPublisher(
 		developerId = BuildConfig.DEVELOPER_ID
+	)
+
+	override val advertiser = MagnetAdvertiser(
+		testMode = BuildConfig.DEBUG,
+		fullAdUnitId = BuildConfig.MAGNET_FULL_TOKEN,
+		bannerAdUnitIds = *arrayOf(
+			Triple(null, BuildConfig.MAGNET_BANNER_TOKEN, null),
+			Triple(
+				Advertiser.UNIT_CONTENT,
+				BuildConfig.MAGNET_CONTENT_TOKEN,
+				MagnetAdvertiser.BANNER_SIZE_MEDIUM_RECTANGLE
+			)
+		)
 	)
 }
